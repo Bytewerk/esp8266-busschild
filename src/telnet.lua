d510775b19bff0f3fbcc384f6ffac1bbe7be3ff3
Peter Brantsch <peter@brantsch.name>
2016-04-01 02:51:04 +0200

    erlaube mehrere „Telnet-Sitzungen“, welche sich den Interpreter teilen

--- src/telnet.lua
+++ src/telnet.lua
@@ -1,21 +1,23 @@
 local telnet = {}
+local sockets = {}
+
+node.output(function(str)
+  for s,_ in pairs(sockets) do
+    s:send(str)
+  end
+end, 0)
 
 function telnet.createServer(port)
   s=net.createServer(net.TCP,180)
   s:listen(port, function(c)
-      function s_output(str)
-        if(c~=nil)
-          then c:send(str)
-        end
-      end
-      node.output(s_output, 0)
-      c:on("receive",function(c,l)
-        node.input(l)
-      end)
-      c:on("disconnection",function(c)
-        node.output(nil)
-      end)
-      print(">")
+    sockets[c] = true
+    c:on("receive",function(c,l)
+      node.input(l)
+    end)
+    c:on("disconnection",function(c)
+      sockets[c] = nil
+    end)
+    print(">")
   end)
   return s
 end
